d14b781dc7ca24672e02c1ba6b76990f93692d22
klvilleta <klvillegas@fondoesperanza.cl>
2023-07-03 13:14:02 -0400

    corrigiendo

--- app/Http/Livewire/Components/Login.php
+++ app/Http/Livewire/Components/Login.php
@@ -4,101 +4,100 @@ namespace App\Http\Livewire\Components;
 
 use App\Http\Controllers\ActivityLogController;
 use App\Models\User;
-use Livewire\Component;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+use Livewire\Component;
 
-class Login extends Component
-{
+class Login extends Component {
 
-    public $show;
+	public $show;
 
-    public function render()
-    {
-        $this->show = true;
-        return view('livewire.components.login', ['show' => $this->show]);
-    }
+	public function render() {
+		$this->show = true;
+		return view('livewire.components.login', ['show' => $this->show]);
+	}
 
-    public function rules()
-    {
-        $rules = [
-            'username' => 'required|min:3|max:50',
-            'password' => 'required|min:6|max:8|regex:"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"',
-        ];
+	public function rules() {
+		$rules = [
+			'username' => 'required|min:3|max:50',
+			'password' => 'required|min:6|max:8|regex:"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"',
+		];
 
-        return $rules;
-    }
+		return $rules;
+	}
 
-    public function messages()
-    {
-        $messages = [
-            'username.required' => 'Usuario requerido',
-            'password.required' => 'Contraseña requerida',
-            'username.min' => 'Usuario debe ser mayor a 3 caracteres',
-            'username.max' => 'Usuario debe ser menor a 50 caracteres',
-            'password.min' => 'Contraseña debe ser mayor a 6 caracteres',
-            'password.max' => 'Contraseña debe ser menor a 8 caracteres',
-            'password.regex' => 'Formato de contraseña  incorrecto',
+	public function messages() {
+		$messages = [
+			'username.required' => 'Usuario requerido',
+			'password.required' => 'Contraseña requerida',
+			'username.min' => 'Usuario debe ser mayor a 3 caracteres',
+			'username.max' => 'Usuario debe ser menor a 50 caracteres',
+			'password.min' => 'Contraseña debe ser mayor a 6 caracteres',
+			'password.max' => 'Contraseña debe ser menor a 8 caracteres',
+			'password.regex' => 'Formato de contraseña  incorrecto',
 
-        ];
-        return $messages;
-    }
+		];
+		return $messages;
+	}
 
-    public function login(Request $request)
-    {
+	public function login(Request $request) {
 
-        
-        $validator = Validator::make($request->all(), $this->rules(), $this->messages());
+		$validator = Validator::make($request->all(), $this->rules(), $this->messages());
 
-        if($validator->fails())
-        {
+		if ($validator->fails()) {
 
-            return Redirect::to('/')->withErrors($validator);
+			return Redirect::to('/')->withErrors($validator);
 
-        }else{
+		} else {
 
-            try {
-                // Verificamos que el usuario exita en base de datos
-                $user = User::where('email', $request->username)->get();
-                foreach($user as $item){
-                    $pass = $item->password;
-                }
+			try {
+				// Verificamos que el usuario exita en base de datos
+				$user = User::where('email', $request->username)->get();
+				foreach ($user as $item) {
+					$pass = $item->password;
+				}
 
-                if($user =! null)
-                {
-                    if (Hash::check($request->password, $pass)){
+				if ($user = !null) {
+					if (Hash::check($request->password, $pass)) {
 
-                        $credenciales = [
-                            'email' => $request->username,
-                            'password' => $request->password,
-                        ];
+						$credenciales = [
+							'email' => $request->username,
+							'password' => $request->password,
+						];
 
-                        Auth::attempt($credenciales);
+						Auth::attempt($credenciales);
+						$request->session()->regenerate();
 
-                        $action = '1';
+						$action = '1';
+						ActivityLogController::store_log($action);
 
-                        ActivityLogController::store_log($action);
+						$user = Auth::user();
+						$status_register = $user->status_register;
 
-                        return Redirect::route('DashboardComponent');
+						// Redireccion segun status de registro
+						if ($status_register == '1') {
+							return view('livewire.components.profile', compact('user'));
 
-                    }
+						} else {
 
-                
-                }else{
-        
-                    return back()->with('error', 'User created successfully.');
-                }
+							return Redirect::route('DashboardComponent');
+						}
+					}
 
-            } catch (\Throwable $th) {
-                
-                Log::error(['exeption in Livewire.Components.Login()', $th]);
-            }
+				} else {
 
-        }
-    }
+					return back()->with('error', 'User created successfully.');
+				}
+
+			} catch (\Throwable $th) {
+				$message = $th->getMessage();
+				dd('Error Livewire.Components.Login.login()', $message);
+			}
+
+		}
+	}
 }
--- app/Http/Livewire/Components/Register.php
+++ app/Http/Livewire/Components/Register.php
@@ -3,76 +3,116 @@
 namespace App\Http\Livewire\Components;
 
 use App\Http\Controllers\ActivityLogController;
+use App\Http\Controllers\UtilsController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
-class Register extends Component
-{
+class Register extends Component {
 
-    public $show;
+	public $show;
 
-    public function store(Request $request)
-    {
-        /**
-         * @param name
-         * @param lastname
-         * @param email
-         * @param password
-         * 
-         * Reglas de validacion + mensajes de validacion.
-         * 
-         * NOTA: El resto de la informacion del usuario sera cargada desde el modulo de configuracion
-         * al completar el registro
-         */
-        $request->validate([
+	public function store(Request $request) {
+		/**
+		 * @param name
+		 * @param lastname
+		 * @param email
+		 * @param password
+		 *
+		 * Reglas de validacion + mensajes de validacion.
+		 *
+		 * NOTA: El resto de la informacion del usuario sera cargada desde el modulo de configuracion
+		 * al completar el registro
+		 */
+		$request->validate([
 
-            'name'      => 'required|min:3|max:50',
-            'last_name' => 'required|min:3|max:50',
-            'email'     => 'required|email|unique:users',
-            'password'  => 'required|min:6|max:8|regex:"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"',
-        ],[
+			'name' => 'required|min:3|max:50',
+			'last_name' => 'required|min:3|max:50',
+			'email' => 'required|email|unique:users',
+			'password' => 'required|min:6|max:8|regex:"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$"',
+		], [
 
-            'name'              => 'Campo requerido',
-            'lastname'          => 'Campo requerido',
-            'email'             => 'Campo requerido',
-            'password'          => 'Campo requerido',
-            'password.min'      => 'Contraseña debe ser mayor a 6 caracteres',
-            'password.max'      => 'Contraseña debe ser menor a 8 caracteres',
-            'password.regex'    => 'Formato de contraseña  incorrecto',
-        ]);
+			'name' => 'Campo requerido',
+			'lastname' => 'Campo requerido',
+			'email' => 'Campo requerido',
+			'email.unique' => 'El email ya se encuentra registrado',
+			'password' => 'Campo requerido',
+			'password.min' => 'Contraseña debe ser mayor a 6 caracteres',
+			'password.max' => 'Contraseña debe ser menor a 8 caracteres',
+			'password.regex' => 'Formato de contraseña  incorrecto',
+		]);
 
-        try {
-                $user = User::create([
+		try {
+			$user = User::create([
 
-                    'name'      => $request->name,
-                    'last_name' => $request->last_name, 
-                    'email'     => $request->email, 
-                    'password'  => Hash::make($request->password),
-                    'role'       => $request->rol, 
-        
-                ]);
-        
-                $action = '3';
+				'name' => $request->name,
+				'last_name' => $request->last_name,
+				'email' => $request->email,
+				'password' => Hash::make($request->password),
+				'role' => $request->rol,
 
-                ActivityLogController::store_log($action);
-        
-                return back()->with('success', 'El registro inicial satisfactorio');
+			]);
 
-        } catch (\Throwable $th) {
-            Log::info(['exeption in Livewire.Components.Login()', $th]);
-        }  
+			$action = '3';
 
-    }
+			ActivityLogController::store_log($action);
 
+			return redirect('/')->with('success', 'El registro inicial satisfactorio');
 
-    public function render()
-    {
-        $this->show = true;
-        return view('livewire.components.register',['show' => $this->show]);
-    }
+		} catch (\Throwable $th) {
+			$message = $th->getMessage();
+			dd('Error Livewire.Components.Register.store()', $message);
+		}
 
+	}
+
+	public function update(Request $request) {
+
+		$request->validate([
+
+			'ci' => 'required',
+			'birthdate' => 'required',
+			'age' => 'required',
+			'phone' => 'required',
+			'state' => 'required',
+			'city' => 'required',
+			'address' => 'required',
+			'zip_code' => 'required',
+		], [
+
+			'ci' => 'Campo requerido',
+			'birthdate' => 'Campo requerido',
+			'age' => 'Campo requerido',
+			'phone' => 'Campo requerido',
+			'state' => 'Campo requerido',
+			'city' => 'Campo requerido',
+			'address' => 'Campo requerido',
+			'zip_code' => 'Campo requerido',
+		]);
+
+		try {
+			// informacion del usuario
+			$user = Auth::user();
+
+			UtilsController::update_registro($user->id, $request);
+
+			$action = '4';
+			ActivityLogController::store_log($action);
+
+			return redirect('DashboardComponent')->with('success', 'Registro actualizado de forma satisfactoria');
+
+		} catch (\Throwable $th) {
+			$message = $th->getMessage();
+			dd('Error Livewire.Components.Register.store()', $message);
+		}
+
+	}
+
+	public function render() {
+		$this->show = true;
+		return view('livewire.components.register', ['show' => $this->show]);
+	}
 
 }
