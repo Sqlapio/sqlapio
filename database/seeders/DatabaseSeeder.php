855f34d77c211b3b7132911bcfb5b1fee3c32e60
Gustavo <administrador@sqlapio.com>
2023-07-04 17:14:16 -0400

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(CenterList::class);
+        $this->call(PathologyList::class);
+        $this->call(StateList::class);
+        $this->call(SpecialtyList::class);
+        $this->call(UserAdmin::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
