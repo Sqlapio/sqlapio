ac3f49b76629a9236f13da2c9fe919910c5e4a48
klvilleta <klvillegas@fondoesperanza.cl>
2023-07-14 01:15:14 -0400

    quitando componente imagen

--- resources/views/livewire/components/profile.blade.php
+++ resources/views/livewire/components/profile.blade.php
@@ -263,11 +263,7 @@
                                                         <i class="bi bi-geo"></i>
                                                 </div>
                                             </diV>
-                                        </div>
-
-                                        {{-- <x-upload-image /> --}}
-
-                                        {{-- <hr> --}}
+                                        </div>                                       
                                         <div class="row mt-3 justify-content-md-end">
                                             <div class="col-sm-4 md-4 lg-4 xl-4 xxl-4">
                                                 <input class="btn btnPrimary send " value="Guardar" type="submit" />
