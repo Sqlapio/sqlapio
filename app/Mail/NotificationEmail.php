1e26e7e824a6b8f66e63934bd14f23447d7a8cfe
Gustavo <administrador@sqlapio.com>
2023-10-07 10:58:12 -0400

    uso de queue en envios de email

--- app/Mail/NotificationEmail.php
+++ app/Mail/NotificationEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationEmail extends Mailable
+class NotificationEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
