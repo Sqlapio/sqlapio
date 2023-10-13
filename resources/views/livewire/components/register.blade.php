ec6442a30637864fde122f2af66e46526c32c421
klvilleta <klvillegas@fondoesperanza.cl>
2023-10-13 10:17:58 -0400

    actualizando

--- resources/views/livewire/components/register.blade.php
+++ resources/views/livewire/components/register.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Registro de Usuario')
 <style>
-
     .mt {
         margin-top: 10rem !important;
     }
@@ -18,21 +17,23 @@
         margin-bottom: -85px !important;
 
     }
-    #div-dos{
+
+    #div-dos {
         margin-right: 110px;
     }
 
     .btn-bg img {
-	    display: block;
+        display: block;
         width: 350px;
     }
+
     .btn-bg img:last-of-type {
         position: absolute;
         top: 224px;
         bottom: 0;
         opacity: 0;
     }
-  
+
     .btn-bg:hover img {
         opacity: 1;
         /* position: relative; */
@@ -43,7 +44,7 @@
     }
 
 
-    
+
 
     @media only screen and (max-width: 768px) {
 
@@ -51,12 +52,13 @@
             display: flex;
             justify-content: center;
         }
+
         .btn-bg img {
             width: 255px;
         }
 
         .btn-bg img:last-of-type {
-        opacity: 0;
+            opacity: 0;
         }
 
         .btn2 {
@@ -75,16 +77,22 @@
             height: auto;
             margin-top: -14% !important;
         }
-        #div-dos{
+
+        #div-dos {
             margin-right: 0px;
         }
+
         .btn-bg {
             display: flex;
             justify-content: center;
         }
 
-    }
+        .btn-bg img:last-of-type {
+            top: 325px;
 
+        }
+
+    }
 </style>
 @push('scripts')
     <script>
@@ -229,34 +237,24 @@
                     <div class="container" style="display: flex; justify-content: center;">
                         <div id="btn-rol" class="row justify-content-center">
                             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 btn-bg">
-                                <img 
-                                data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-custom-class="custom-tooltip" data-html="true"
-                                title="Registrar como médico"                          
-                                onclick="handlerRol('medico')" 
-                                src="{{ asset('/img/V2/Boton_medico.png') }}"
-                                height="auto" alt="avatar">
-                                <img 
-                                data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-custom-class="custom-tooltip" data-html="true"
-                                title="Registrar como médico"                          
-                                onclick="handlerRol('medico')" src="{{ asset('/img/V2/Boton_medico_bg.png') }}"
-                                height="auto" alt="avatar">
+                                <img data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                    data-bs-custom-class="custom-tooltip" data-html="true" title="Registrar como médico"
+                                    onclick="handlerRol('medico')" src="{{ asset('/img/V2/Boton_medico.png') }}"
+                                    height="auto" alt="avatar">
+                                <img data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                    data-bs-custom-class="custom-tooltip" data-html="true" title="Registrar como médico"
+                                    onclick="handlerRol('medico')" src="{{ asset('/img/V2/Boton_medico_bg.png') }}"
+                                    height="auto" alt="avatar">
                             </div>
                             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 btn-bg">
-                                <img
-                                data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-custom-class="custom-tooltip" data-html="true"
-                                title="Registrar como laboratorio"                                                       
-                                onclick="handlerRol('laboratorio')" 
-                                src="{{ asset('/img/V2/Boton_laboratorio.png') }}"
-                                     height="auto" alt="avatar">
-                                <img
-                                data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-custom-class="custom-tooltip" data-html="true"
-                                title="Registrar como laboratorio"                                                       
-                                onclick="handlerRol('laboratorio')" src="{{ asset('/img/V2/Boton_laboratorio_bg.png') }}"
-                                     height="auto" alt="avatar">
+                                <img data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                    data-bs-custom-class="custom-tooltip" data-html="true"
+                                    title="Registrar como laboratorio" onclick="handlerRol('laboratorio')"
+                                    src="{{ asset('/img/V2/Boton_laboratorio.png') }}" height="auto" alt="avatar">
+                                <img data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                    data-bs-custom-class="custom-tooltip" data-html="true"
+                                    title="Registrar como laboratorio" onclick="handlerRol('laboratorio')"
+                                    src="{{ asset('/img/V2/Boton_laboratorio_bg.png') }}" height="auto" alt="avatar">
                             </div>
                         </div>
                     </div>
@@ -267,7 +265,8 @@
                             <div>
                                 <div class="container">
                                     <div class="row mt-3" style="display: grid; justify-items: center;">
-                                        <img class="logoSq" src="{{ asset('img/logo sqlapio variaciones-02.png') }}" alt="">
+                                        <img class="logoSq" src="{{ asset('img/logo sqlapio variaciones-02.png') }}"
+                                            alt="">
                                     </div>
                                 </div>
                                 {{ Form::open(['url' => 'register', 'method' => 'post', 'id' => 'form-register']) }}
@@ -276,7 +275,8 @@
                                     <div id="name-input" class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Nombres</label>
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Nombres</label>
                                                 <input autocomplete="off"
                                                     class="form-control mask-text @error('name') is-invalid @enderror"
                                                     id="name" name="name" type="text" value="">
@@ -288,7 +288,9 @@
                                     <div id="business-name" class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Nombre del Laboratorio</label>
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Nombre del
+                                                    Laboratorio</label>
                                                 <input autocomplete="off"
                                                     class="form-control mask-text @error('business_name') is-invalid @enderror"
                                                     id="business_name" name="business_name" type="text" value="">
@@ -299,20 +301,22 @@
                                     <div id="apellidos" class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Apellidos</label>
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Apellidos</label>
                                                 <input autocomplete="off"
                                                     class="form-control mask-text @error('last_name') is-invalid @enderror"
                                                     id="last_name" name="last_name" type="text" value="">
-                                                <i class="bi bi-person-circle st-icon" ></i>
+                                                <i class="bi bi-person-circle st-icon"></i>
                                             </div>
                                         </diV>
                                     </div>
                                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Correo Electrónico</label>
-                                                <input autocomplete="off"
-                                                    class="form-control" id="email"
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Correo
+                                                    Electrónico</label>
+                                                <input autocomplete="off" class="form-control" id="email"
                                                     name="email" type="text" value="">
                                                 <i class="bi bi-envelope st-icon"></i>
                                             </div>
@@ -321,7 +325,8 @@
                                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Rol</label>
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Rol</label>
                                                 <input autocomplete="off" placeholder="Rol"
                                                     class="form-control @error('rol') is-invalid @enderror" id="rol"
                                                     name="rol" type="text" value="" readonly="readonly">
@@ -333,17 +338,19 @@
                                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Contraseña</label>
-                                                <input placeholder="Contraseña" autocomplete="off" data-bs-toggle="tooltip"
-                                                        data-bs-placement="right" data-bs-custom-class="custom-tooltip tooltip-ps" data-html="true"
-                                                        title="La contraseña debe contener:
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Contraseña</label>
+                                                <input placeholder="Contraseña" autocomplete="off"
+                                                    data-bs-toggle="tooltip" data-bs-placement="right"
+                                                    data-bs-custom-class="custom-tooltip tooltip-ps" data-html="true"
+                                                    title="La contraseña debe contener:
                                                             Al menos una letra mayúscula.
                                                             Al menos una letra minúscula. 
                                                             Al menos un número.
                                                             Mínimo 6 carácteres.
                                                             Máximo 8 carácteres"
-                                                            class="form-control @error('password') is-invalid @enderror" 
-                                                            id="password" name="password" type="password" value="">
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    id="password" name="password" type="password" value="">
                                                 <i onclick="showPass();" class="bi bi-eye-fill st-icon"></i>
                                             </div>
                                         </diV>
@@ -351,7 +358,9 @@
                                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                         <div class="form-group">
                                             <div class="Icon-inside">
-                                                <label for="name" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Confirmar Contraseña</label>
+                                                <label for="name" class="form-label"
+                                                    style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">Confirmar
+                                                    Contraseña</label>
                                                 <input autocomplete="off" placeholder="Confirmar Contraseña"
                                                     class="form-control @error('password_confrimation') is-invalid @enderror"
                                                     id="password_confrimation" name="password_confrimation"
@@ -367,9 +376,11 @@
                                     </div>
                                 </div>
                                 <div class="d-flex justify-content-center">
-                                    <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl--8 mt-3 mb-3" style="display: flex; justify-content: space-around;">
+                                    <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 col-xxl--8 mt-3 mb-3"
+                                        style="display: flex; justify-content: space-around;">
                                         <button type="" class="btn btnPrimary">Registrar</button>
-                                        <a href="/"><button type="button" class="btn btnSecond btn2">Cancelar</button></a>
+                                        <a href="/"><button type="button"
+                                                class="btn btnSecond btn2">Cancelar</button></a>
                                     </div>
                                 </div>
                                 {{ Form::close() }}
