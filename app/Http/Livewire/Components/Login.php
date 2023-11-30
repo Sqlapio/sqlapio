3dab87ff5998861ea6fc906c4a31c3c128c44754
klvilleta <klvillegas@fondoesperanza.cl>
2023-11-30 14:52:03 -0400

    actualizndo

--- app/Http/Livewire/Components/Login.php
+++ app/Http/Livewire/Components/Login.php
@@ -54,51 +54,53 @@ class Login extends Component
 		} else {
 
 			try {
-				// Verificamos que el usuario exita en base de datos
 				$user_exist = User::where('email', $request->username)->first();
-
+				// Verificamos que el usuario exita en base de datos
 				if ($user_exist != null) {
-					if (Hash::check($request->password, $user_exist->password) && $user_exist->tipo_status == '1')
-                    {
+					// Verificamos status del usuriao habilitadoi
+					if ($user_exist->tipo_status == '1') {
+						if (Hash::check($request->password, $user_exist->password)) {
 
-						$credenciales = [
-							'email' => $request->username,
-							'password' => $request->password,
-						];
+							$credenciales = [
+								'email' => $request->username,
+								'password' => $request->password,
+							];
 
-						Auth::attempt($credenciales);
-						$request->session()->regenerate();
+							Auth::attempt($credenciales);
+							$request->session()->regenerate();
 
-						$action = '1';
-						ActivityLogController::store_log($action);
+							$action = '1';
+							ActivityLogController::store_log($action);
 
-						$user = Auth::user();
+							$user = Auth::user();
 
-						// verificar si vlaido el correo
-						if ($user->email_verified_at === null) {
-							return Redirect::to('/')->withErrors('Debe verificar su correo electronico!');
-						}
-
-						$status_register = $user->status_register;
-						$speciality = Specialty::all();
-
-						// Redireccion segun status de registro
-						if ($status_register == '1') {
-							return view('livewire.components.profile', compact('user', 'speciality'));
-						} else {
-
-							if($user->role=="corporativo"){
-								return Redirect::route('Dashboard-corporate');
-
-							}else{
-								return Redirect::route('DashboardComponent');
+							// verificar si vlaido el correo
+							if ($user->email_verified_at === null) {
+								return Redirect::to('/')->withErrors('Debe verificar su correo electronico!');
 							}
 
-						}
-					}
-				} else {
+							$status_register = $user->status_register;
+							$speciality = Specialty::all();
 
-					return back()->with('error', 'El usuario no existe o se encuentra deshabilitado. Por favor valide la información y vuelva a intentarlo');
+							// Redireccion segun status de registro
+							if ($status_register == '1') {
+								return view('livewire.components.profile', compact('user', 'speciality'));
+							} else {
+
+								if ($user->role == "corporativo") {
+									return Redirect::route('Dashboard-corporate');
+								} else {
+									return Redirect::route('DashboardComponent');
+								}
+							}
+						} else { // credenciales incorrectas
+							return Redirect::to('/')->withErrors('Autenticación incorrecta');
+						}
+					} else { // usuario desahabilitado
+						return Redirect::to('/')->withErrors('El usuario no existe o se encuentra deshabilitado. Por favor valide la información y vuelva a intentarlo');
+					}
+				} else {//no exite usuario
+					return Redirect::to('/')->withErrors('Autenticación incorrecta');
 				}
 			} catch (\Throwable $th) {
 				$message = $th->getMessage();
--- resources/views/livewire/components/profile_corporate/admin-patients.blade.php
+++ resources/views/livewire/components/profile_corporate/admin-patients.blade.php
@@ -1,59 +1,134 @@
 @extends('layouts.app-auth')
 @section('title', 'Gestión Pacientes')
 <style>
-   
+
 </style>
 @push('scripts')
-    <script>
-      
-    </script>
+    <script></script>
 @endpush
 @section('content')
-<div class="container-fluid" style="padding: 0 3% 3%">
-    <div class="row mt-2">
-     
-        <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 mb-cd">
-            <div class="accordion" id="accordion">
-                <div class="accordion-item">
-                    <span class="accordion-header title" id="headingOne">
-                        <button class="accordion-button bg-4" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="width: -webkit-fill-available; width: -moz-available; width: fill-available;">
-                            <i class="bi bi-hospital"></i>Gestión Pacientes
-                        </button>
-                    </span>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
-                        <div class="accordion-body">
-                         
-                            <div class="table-responsive" id="table-patients" style="margin-top: 20px; width: 100%;">
-                                <table id="table-centers" class="table table-striped table-bordered" style="width: 100%;">
-                                    <thead class="table-light">
-                                        <tr>
-                                            <th class="text-center">Nombre de centro</th>
-                                            <th class="text-center">Dirección</th>
-                                            <th class="text-center">Piso</th>
-                                            <th class="text-center">Número de consultorio</th>
-                                            <th class="text-center">Teléfono del consultorio</th>
-                                            <th class="text-center">Estatus</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {{-- @foreach ($doctor_centers as $key => $item)
-                                            <tr>
-                                                <td class="text-center pt-2">{{ $item['center'] }}</td>
-                                                <td class="text-center">{{ $item['address'] }}</td>
-                                                <td class="text-center">{{ $item['number_floor'] }}</td>
-                                                <td class="text-center">{{ $item['number_consulting_room'] }}</td>
-                                                <td class="text-center">{{ $item['phone_consulting_room'] }}</td>
-                                                <td class="text-center table-check w-5">
-                                                    <div class="form-check form-switch ">
-                                                        <input onchange="handlerCenter(event);" style="width: 5em"
-                                                            class="form-check-input" type="checkbox" role="switch"
-                                                            id="flexSwitchCheckChecked" value="{{ $item['id'] }}"
-                                                            {{ $item['status'] != '1' ? '' : 'checked' }}>
-                                                    </div>
-                                            </tr>
-                                        @endforeach --}}
-                                    </tbody>
-                                </table>
+    <div class="container-fluid" style="padding: 0 3% 3%">
+        <div class="row mt-2">
+
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 mb-cd">
+                <div class="accordion" id="accordion">
+                    <div class="accordion-item">
+                        <span class="accordion-header title" id="headingOne">
+                            <button class="accordion-button bg-4" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+                                style="width: -webkit-fill-available; width: -moz-available; width: fill-available;">
+                                <i class="bi bi-hospital"></i>Gestión Pacientes
+                            </button>
+                        </span>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                            data-bs-parent="#accordion">
+                            <div class="accordion-body">
+                                {{-- Lista de pacientes con consultas  --}}
+                                <div class="row" id="table-patients">
+                                    <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 table-responsive"
+                                        style="margin-top: 20px;">
+                                        <table id="table-patient" class="table table-striped table-bordered"
+                                            style="width:100%; ">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center" scope="col">Imagen</th>
+                                                    <th class="text-center" scope="col">Código
+                                                        paciente</th>
+                                                    <th class="text-center" scope="col">Nombre</th>
+                                                    <th class="text-center" scope="col">Cédula</th>
+                                                    <th class="text-center" scope="col">Fecha de
+                                                        Nacimiento </th>
+                                                    <th class="text-center" scope="col">Género</th>
+                                                    <th class="text-center" scope="col">Teléfono</th>
+                                                    <th class="text-center" scope="col">Email</th>
+                                                    <th class="text-center" scope="col">Centro de
+                                                        salud</th>
+                                                    <th class="text-center"scope="col">Acciones</th>
+
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {{-- @foreach ($patients as $item)
+                                    <tr>
+                                        <td class="table-avatar">
+                                            <img class="avatar"
+                                                src=" {{ $item->get_paciente->patient_img ? asset('/imgs/' . $item->get_paciente->patient_img) : ($item->get_paciente->genere == 'femenino' ? asset('/img/avatar/avatar mujer.png') : asset('/img/avatar/avatar hombre.png')) }}"
+                                                alt="Imagen del paciente">
+                                        </td>
+                                        <td class="text-center">
+                                            <button
+                                                onclick="agendarCita({{ $item->get_paciente }},{{ $item->get_paciente->get_reprensetative }})"
+                                                type="button" class="btn btnSecond"
+                                                data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                                data-bs-custom-class="custom-tooltip" data-html="true"
+                                                title="Agendar cita">{{ $item->get_paciente->patient_code }}</button>
+                                        </td>
+                                        <td class="text-center text-capitalize">
+                                            {{ $item->get_paciente->name }}
+                                            {{ $item->get_paciente->last_name }}</td>
+                                        <td class="text-center">
+                                            {{ $item->get_paciente->is_minor === 'true' ? $item->get_paciente->get_reprensetative->re_ci . '  (Rep)' : $item->get_paciente->ci }}
+                                        </td>
+                                        <td class="text-center">
+                                            {{ date('d-m-Y', strtotime($item->get_paciente->birthdate)) }}
+                                        </td>
+                                        <td class="text-center text-capitalize">
+                                            {{ $item->get_paciente->genere }}</td>
+                                        <td class="text-center">
+                                            {{ $item->get_paciente->is_minor === 'true' ? $item->get_paciente->get_reprensetative->re_phone . '  (Rep)' : $item->get_paciente->phone }}
+                                        </td>
+                                        <td class="text-center">
+                                            {{ $item->get_paciente->is_minor === 'true' ? $item->get_paciente->get_reprensetative->re_email . '  (Rep)' : $item->get_paciente->email }}
+                                        </td>
+                                        <td class="text-center">
+                                            {{ $item->get_center->description }}</td>
+                                        <td class="text-center">
+                                            <div class="d-flex">
+                                                <div
+                                                    class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                                    <button
+                                                        onclick="editPatien({{ json_encode($item->get_paciente) }},true); "
+                                                        type="button"
+                                                        class="btn btn-iSecond rounded-circle"
+                                                        data-bs-toggle="tooltip"
+                                                        data-bs-placement="bottom" title="Editar">
+                                                        <i class="bi bi-pencil"></i>
+                                                    </button>
+                                                </div>
+                                                <div
+                                                    class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                                    <a
+                                                        href="{{ route('MedicalRecord', $item->get_paciente->id) }}">
+                                                        <button type="button"
+                                                            class="btn btn-iPrimary rounded-circle"
+                                                            data-bs-toggle="tooltip"
+                                                            data-bs-placement="bottom"
+                                                            title="Consulta médica">
+                                                            <i class="bi bi-file-earmark-text"></i>
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                                <div
+                                                    class="col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                                                    <a
+                                                        href="{{ route('ClinicalHistoryDetail', $item->get_paciente->id) }}">
+                                                        <button type="button"
+                                                            class="btn btn-iSecond rounded-circle"
+                                                            data-bs-toggle="tooltip"
+                                                            data-bs-placement="bottom"
+                                                            title="Historia Clínica">
+                                                            <i class="bi bi-file-earmark-text"></i>
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach --}}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -61,6 +136,5 @@
             </div>
         </div>
     </div>
-</div>
 
 @endsection
