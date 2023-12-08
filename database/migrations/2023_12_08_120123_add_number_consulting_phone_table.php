6d1a615a1057071654ead631b26397ac3663e1ce
Gustavo <administrador@sqlapio.com>
2023-12-08 12:03:42 -0400

    modificando la actualizacion del registro v2

--- database/migrations/2023_12_08_120123_add_number_consulting_phone_table.php
+++ database/migrations/2023_12_08_120123_add_number_consulting_phone_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('Laboratories', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('number_consulting_phone')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('Laboratories', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
