199e9474c97524167d316e232334bc9a684f13c9
klvilleta <klvillegas@fondoesperanza.cl>
2024-01-09 10:39:39 -0400

    integrando estructura de carpetas para las fuerzas de ventas

---
+++ app/Http/Livewire/Components/SalesForces/RegisterUserSalesForces.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\SalesForces;
+
+use Livewire\Component;
+
+class RegisterUserSalesForces extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.sales-forces.register-user-sales-forces');
+    }
+}
