cf4c6d0a3c1534b9421e3e4283fb1ef6ad4485dd
Katherine <Karanguren_12@outlook.com>
2024-01-18 17:06:10 -0400

    cambios tablas y notificacion

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('title', 'Confirm')
+<style>
+
+
+</style>
+@section('content')
+    <div>
+        <div class="container-fluid text-center">
+            <div class="row form-sq" style="position: relative">
+                <div class="col-xs-10 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3 loginDric">
+                    <div class="card" id="div-form">
+                        <div class="card-body">
+                            
+                            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <div class="text-center">
+                                    <img class="img" src="{{ asset('img/confirmation.jpg') }}" style="width: 200px;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
