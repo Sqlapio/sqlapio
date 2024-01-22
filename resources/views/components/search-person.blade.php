cf41fd9591e42679d4ebcabec5390aa2914a91d3
Katherine <Karanguren_12@outlook.com>
2024-01-22 10:32:23 -0400

    cambios de estilo v2

--- resources/views/components/search-person.blade.php
+++ resources/views/components/search-person.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="row mt-2">     
-        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mt-2">
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4 mt-4">
             <div class="form-group">
                 <label for="search_person"
                     class="form-label"style="font-size: 13px; margin-bottom: 5px; margin-top: -23px">
