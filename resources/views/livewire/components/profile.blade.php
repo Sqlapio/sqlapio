4a4b3f6d97e5beebe4fba64583e2ff0ca128a2a3
Gustavo <administrador@sqlapio.com>
2024-02-06 11:36:19 -0400

    correcion de idiomas y se agrega modificacion en el label de mpps v6

--- resources/views/livewire/components/profile.blade.php
+++ resources/views/livewire/components/profile.blade.php
@@ -593,7 +593,7 @@
                                                         <div class="form-group">
                                                             <div class="Icon-inside">
                                                                 @if (Auth::user()->contrie == '81')
-                                                                <label for="ci" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">@lang('messages.form.RNC')</label>
+                                                                <label for="ci" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">@lang('messages.form.RCN')</label>
                                                                 @else
                                                                 <label for="ci" class="form-label" style="font-size: 13px; margin-bottom: 5px; margin-top: 4px">@lang('messages.form.cedula_identidad')</label>
                                                                 @endif
