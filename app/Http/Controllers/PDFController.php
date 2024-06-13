503cf2bcfb9e9258b8892d6e5a80dd8abfc4f6d9
karanguren <karanguren_12@outlook.com>
2024-06-13 16:38:55 -0400

    pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -49,7 +49,7 @@ class PDFController extends Controller
         // ];
         // return view("pdf.PDF_medical_record2", compact($data));
         // return $pdf->stream('consulta-medica.pdf');
-        Browsershot::url('https://system.sqlapio.com/pdf/medical-record2/'.$id)
+        Browsershot::url('https://system.sqlapio.com/pdf/medical-record/'.$id)
         ->setNodeBinary('/usr/bin/node')
     	->setNpmBinary('/usr/bin/npm')
         ->setChromePath('/usr/bin/chromium')
