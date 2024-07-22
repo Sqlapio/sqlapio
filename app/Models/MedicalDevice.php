ecf666f631ee7ce221b1f6c69502be1f52f90caa
Gustavo <administrador@sqlapio.com>
2024-07-22 09:43:54 -0400

    subiendo modelo y migracion para la tabla de dispositivos medicos

---
+++ app/Models/MedicalDevice.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MedicalDevice extends Model
+{
+    use HasFactory;
+
+    /**
+     * Define table
+     */
+    protected $table = 'medical_devices';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'description_en',
+    ];
+}
