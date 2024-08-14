d0522e8b4d9fca7861f0561fd93e7ce556673c19
karanguren <karanguren_12@outlook.com>
2024-08-14 16:12:54 -0400

    buscadores

--- app/Models/MedicalRecord.php
+++ app/Models/MedicalRecord.php
@@ -83,4 +83,34 @@ class MedicalRecord extends Model
         return $this->hasMany(StudyPatient::class, 'medical_record_id', 'id')->where('status','2');
     }
 
+    /**
+     * Get all of the get_exams for the MedicalRecord
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function get_exams(): HasMany
+    {
+        return $this->hasMany(ExamPatient::class, 'medical_record_id', 'id');
+    }
+
+    /**
+     * Get all of the get_exams for the MedicalRecord
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function get_studies(): HasMany
+    {
+        return $this->hasMany(StudyPatient::class,  'medical_record_id', 'id');
+    }
+
+    /**
+     * Get all of the get_exams for the MedicalRecord
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function get_tratamientos(): HasMany
+    {
+        return $this->hasMany(Treatment::class,  'record_code', 'record_code');
+    }
+
 }
