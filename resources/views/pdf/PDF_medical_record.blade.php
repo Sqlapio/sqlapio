05b715ca4d4734015f51ee359958d344b4771e27
karanguren <karanguren_12@outlook.com>
2024-08-22 15:32:24 -0400

    pdf

--- resources/views/pdf/PDF_medical_record.blade.php
+++ resources/views/pdf/PDF_medical_record.blade.php
@@ -4,7 +4,7 @@
     @page { margin:0px; }
     body {
         font-family: 'Creato Display', sans-serif;
-        margin-top: 0cm;
+        margin-top: 3cm;
         margin-left: 0cm;
         margin-right: 0cm;
         margin-bottom: 0cm;
@@ -13,7 +13,7 @@
         -moz-background-size: contain;
         -o-background-size: contain;
         background-size: contain !important;
-        background: url({{ asset('img/bg_pdf/green.png') }}) no-repeat top fixed;
+        background: url({{ asset('img/bg_pdf/' . $bg) }}) no-repeat top fixed;
     }
 
     .imagen {
@@ -31,9 +31,17 @@
         border-radius: 10px;
     }
 
+    pre {
+        white-space: pre-wrap;
+        white-space: -moz-pre-wrap;
+        white-space: -pre-wrap;
+        white-space: -o-pre-wrap;
+        word-wrap: break-word;
+        text-align: justify;
+        line-height: 1.4;
+    }
+
     .container-fluid {
-        position: fixed;
-        bottom: 3cm;
         width: 100%;
 
     }
@@ -55,6 +63,7 @@
         white-space: -o-pre-wrap;
         word-wrap: break-word;
         text-align: justify;
+        line-height: 1.4;
     }
 
     header {
@@ -67,7 +76,7 @@
         display: flex;
     }
 
-/** Define the footer rules **/
+
     footer {
         position: fixed;
         bottom: 1cm;
@@ -84,84 +93,106 @@
 @push('scripts')
 @endpush
 @section('content')
-    <div>
-        <header>
-            <span class="text-capitalize" style="font-size: 20px; margin-bottom: 5px">{{ $nombre }}</span></strong><br>
-            <span class="text-capitalize" style="font-size: 15px;">C.I: {{ $ci }} / MPPS: {{ $mpps }}</span><br>
-            <span class="text-capitalize" style="font-size: 15px;">Especialidad: {{ $especialidad }}</span>
-        </header>
-        <div class="container-fluid">
-            <div class="" style="margin-top: 16%;">
-                <hr style="color:#0000001a">
-                <div class="row-data" style="display: flex; width: 100%">
-                    <div style="flex: 50%; font-size: 15px">
-                        {{-- Datos del paciente --}}
-                        <div class="col-md-6">
-                            <strong>Nombre:
-                            </strong><span class="text-capitalize">{{ $MedicalRecord->get_paciente->name . ' ' . $MedicalRecord->get_paciente->last_name }}</span>
-                            <br>
-                            <strong>C.I:</strong> <span class="text-capitalize">{{ $MedicalRecord->get_paciente->ci }}</span>
-                            <br>
-                            <strong>Género:</strong> <span class="text-capitalize">{{ $MedicalRecord->get_paciente->genere }}</span>
-                            <strong>Edad:</strong> <span>{{ $MedicalRecord->get_paciente->age }}</span>
-                            <br>
-                            <strong>Correo electrónico:</strong>
-                            <span>{{ $MedicalRecord->get_paciente->email }}</span>
-                            <br>
-                            <strong>Teléfono:</strong> <span>{{ $MedicalRecord->get_paciente->phone }}</span>
-                            <br>
-                            <strong>Dirección:</strong>
-                            <span>{{ $MedicalRecord->get_paciente->address }}</span>
-                            <br>
-                            <strong>Código:</strong>
-                            <span>{{ $MedicalRecord->get_paciente->patient_code }}</span>
-                        </div>
-                    </div>
-                    {{-- <div style="flex: 50%;">
-                        <div style="text-align: center">
-                            <img class="barcodeStyle" src="data:image/png;base64,{{ $barcode }}"><br>
-                            <span class="code-span">{{ $MedicalRecord->get_paciente->patient_code }}</span>
-                        </div>
-                    </div> --}}
-                </div>
-                <div class="row-barcode">
-                    <div class="text-center" style="text-align: center; margin-top: 30px; font-size: 21px">
-                        <strong> Informe Médico de Consulta</strong>
-                    </div>
-                </div>
-                <div class="row-data">
-                    <div style="margin-top: 30px">
-                        <strong>Razón de la visita:</strong>
-                        <pre style="font-size: 17px ; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">{{ $MedicalRecord->razon }}</pre>
+<body>
+    <header>
+        <span class="text-capitalize" style="font-size: 20px; margin-bottom: 5px">{{ $nombre }}</span></strong><br>
+        <span class="text-capitalize" style="font-size: 15px;">C.I: {{ $ci }} / MPPS: {{ $mpps }}</span><br>
+        <span class="text-capitalize" style="font-size: 15px;">Especialidad: {{ $especialidad }}</span>
+    </header>
+
+    <footer class="footer">
+        <div>
+            <div style="margin-bottom: 5px; text-align: center;">
+                <p>Direccion: {{ $direccion }}. Piso {{ $piso }}, Consultorio {{ $consultorio_num }}
+                    <br>Telefono: {{ $consultorio_tel }} / {{ $personal_tel }}
+                </p>
+            </div>
+            <div class="pagenum-container">Page <span class="pagenum"></span></div>
+        </div>
+    </footer>
+
+    <div class="container-fluid">
+        <div>
+            <hr style="color:#0000001a">
+            <div class="row-data" style="display: flex; width: 100%">
+                <div style="flex: 50%; font-size: 15px">
+                    {{-- Datos del paciente --}}
+                    <div class="col-md-6">
+                        <strong>Nombre:
+                        </strong><span class="text-capitalize">{{ $MedicalRecord->get_paciente->name . ' ' . $MedicalRecord->get_paciente->last_name }}</span>
                         <br>
-                        <strong>Diagnóstico:</strong>
-                        @php
-                            $des = str_replace('Diagnóstico: ', '', $MedicalRecord->diagnosis);
-                            $description = str_replace('\r\n', '<br>', $des)
-                        @endphp
-                            <pre style="font-size: 17px ; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">{{ $description }}</pre>
+                        <strong>C.I:</strong> <span class="text-capitalize">{{ $MedicalRecord->get_paciente->ci }}</span>
                         <br>
-                        <strong>Sintomas:</strong>
-                        <p style="font-size: 17px">{{ $MedicalRecord->sintomas }}</p>
+                        <strong>Género:</strong> <span class="text-capitalize">{{ $MedicalRecord->get_paciente->genere }}</span>
+                        <strong>Edad:</strong> <span>{{ $MedicalRecord->get_paciente->age }}</span>
                         <br>
-                        <strong>Tratamiento:</strong>
-                        @foreach (json_decode($MedicalRecord->medications_supplements) as $item)
-                            <p style="font-size: 17px"><strong>Medicamento:</strong> {{ $item->medicine }}, <strong>Indicaciones:</strong> {{ $item->indication }}, <strong>Via:</strong> {{ $item->route }}, <strong>Duración:</strong> {{ $item->treatmentDuration}}, cada {{ $item->hours }} Horas.</p>
-                        @endforeach
+                        <strong>Correo electrónico:</strong>
+                        <span>{{ $MedicalRecord->get_paciente->email }}</span>
                         <br>
+                        <strong>Teléfono:</strong> <span>{{ $MedicalRecord->get_paciente->phone }}</span>
+                        <br>
+                        <strong>Dirección:</strong>
+                        <span>{{ $MedicalRecord->get_paciente->address }}</span>
+                        <br>
+                        <strong>Código:</strong>
+                        <span>{{ $MedicalRecord->get_paciente->patient_code }}</span>
                     </div>
                 </div>
+                {{-- <div style="flex: 50%;">
+                    <div style="text-align: center">
+                        <img class="barcodeStyle" src="data:image/png;base64,{{ $barcode }}"><br>
+                        <span class="code-span">{{ $MedicalRecord->get_paciente->patient_code }}</span>
+                    </div>
+                </div> --}}
+            </div>
+            <div class="row-barcode">
+                <div class="text-center" style="text-align: center; margin-top: 30px; font-size: 21px">
+                    <strong> Informe Médico de Consulta</strong>
+                </div>
+            </div>
+            <div class="row-data">
+                <div style="margin-top: 30px">
+                    <strong>Razón de la visita:</strong>
+                    <pre style="font-size: 17px ; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">{{ $MedicalRecord->razon }}</pre>
+                    <br>
+                </div>
+            </div>
+            @if(strlen($MedicalRecord->razon)>1000)
+                <div style="page-break-after:always;"></div>
+            @endif
+            <div class="row-data">
+                <strong>Diagnóstico:</strong>
+                @php
+                    $des = str_replace('Diagnóstico: ', '', $MedicalRecord->diagnosis);
+                    $description = str_replace('\r\n', "<br>", $des)
+                @endphp
+                    <pre style="font-size: 17px ; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">{{ $description }}</pre>
+            </div>
+            <br>
+            @if(strlen($MedicalRecord->diagnosis)>600)
+                <div style="page-break-after:always; margin-top: 16%;"></div>
+            @endif
+            <div class="row-data">
+                <strong>Sintomas:</strong>
+                <p style="font-size: 17px">{{ $MedicalRecord->sintomas }}</p>
+                <br>
+                <strong>Exámenes:</strong>
+                @foreach ($MedicalRecord->get_exam_medical as $item)
+                <p style="font-size: 17px">{{ $item->description }}</p>
+                @endforeach
+                <br>
+                <strong>Estudios:</strong>
+                @foreach ($MedicalRecord->get_study_medical as $item)
+                <p style="font-size: 17px">{{ $item->description }}</p>
+                @endforeach
+                <br>
+                <strong>Tratamiento:</strong>
+                @foreach (json_decode($MedicalRecord->medications_supplements) as $item)
+                    <p style="font-size: 17px"><strong>Medicamento:</strong> {{ $item->medicine }}, <strong>Indicaciones:</strong> {{ $item->indication }}, <strong>Via:</strong> {{ $item->route }}, <strong>Duración:</strong> {{ $item->treatmentDuration}}, cada {{ $item->hours }} Horas.</p>
+                @endforeach
+                <br>
             </div>
         </div>
-        <footer style="text-align: center;">
-            <div>
-                <div style="margin-bottom: 5px; text-align: center;">
-                    <p>Direccion: {{ $direccion }}. Piso {{ $piso }}, Consultorio {{ $consultorio_num }}
-                        <br>Telefono: {{ $consultorio_tel }} / {{ $personal_tel }}
-                    </p>
-                </div>
-                <div class="pagenum-container">Page <span class="pagenum"></span></div>
-            </div>
-        </footer>
     </div>
+</body>
 @endsection
