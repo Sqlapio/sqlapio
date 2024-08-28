23d87f6298fdd5fafd0d5735fb17fc043d07ea98
karanguren <karanguren_12@outlook.com>
2024-08-28 14:55:47 -0400

    notificaciones

--- resources/views/cancel.blade.php
+++ resources/views/cancel.blade.php
@@ -2,23 +2,51 @@
 @section('title', 'Cancel')
 <style>
 
-#background-video {
-   width: 100vw;
-   height: 100vh;
-   object-fit: cover;
-   position: fixed;
-   left: 0;
-   right: 0;
-   top: 0;
-   bottom: 0;
-   z-index: -1;
-}
+    #background-video {
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            z-index: -1;
+    }
+
+    @media only screen and (min-width: 768px) {
+        #background-video-mobile {
+            display: none;
+        }
+    }
+
+
+    @media only screen and (max-width: 768px) {
+        #background-video-mobile {
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            z-index: -1;
+        }
+
+        #background-video {
+            display: none;
+        }
+    }
 
 </style>
 @section('content')
     <div>
         <video id="background-video" autoplay loop muted>
-            <source src="img/cancelada.mp4" type="video/mp4">
+            <source src="{{ asset('img/cancelada.mp4') }}" type="video/mp4">
+        </video>
+        <video id="background-video-mobile" autoplay loop muted>
+            <source src="{{ asset('img/CANCELADO_APP.mp4') }}" type="video/mp4">
         </video>
     </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Confirm')
 <style>
-body {
-        font-family: 'Creato Display', sans-serif;
-        margin-top: 0cm;
-        margin-left: 0cm;
-        margin-right: 0cm;
-        margin-bottom: 0cm;
 
-        -webkit-background-size: 100% 100%;
-        -moz-background-size: 100% 100%;
-        -o-background-size: 100% 100%;
-        background-size: 100% 100% !important;
-        background: url({{ asset('img/confirmada.mp4') }});
-    }
 
     #background-video {
         width: 100vw;
@@ -27,11 +15,39 @@ body {
         z-index: -1;
     }
 
+    @media only screen and (min-width: 768px) {
+        #background-video-mobile {
+            display: none;
+        }
+    }
+
+
+    @media only screen and (max-width: 768px) {
+        #background-video-mobile {
+            width: 100vw;
+            height: 100vh;
+            object-fit: cover;
+            position: fixed;
+            left: 0;
+            right: 0;
+            top: 0;
+            bottom: 0;
+            z-index: -1;
+        }
+
+        #background-video {
+            display: none;
+        }
+    }
+
 </style>
 @section('content')
     <div>
         <video id="background-video" autoplay loop muted>
-            <source src="img/confirmada.mp4" type="video/mp4">
+            <source src="{{ asset('img/confirmada.mp4') }}" type="video/mp4">
+        </video>
+        <video id="background-video-mobile" autoplay loop muted>
+            <source src="{{ asset('img/FONDO_APP.mp4') }}" type="video/mp4">
         </video>
     </div>
 @endsection
